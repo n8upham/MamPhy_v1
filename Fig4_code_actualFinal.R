f74d9d60dcebbfdf1e5c438cb287e65c893de23b
n8upham <n8upham@users.noreply.github.com>
2017-09-17 23:02:43 -0400

    Load per-fig code templates

---
+++ Fig4_code_actualFinal.R
@@ -0,0 +1,9 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# R Code - MamPhy -- Upham et al. 2017
+###
+# Figure 4 - time slices to explain clade diversification rate
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
